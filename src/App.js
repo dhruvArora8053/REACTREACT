9c05abded94fd8a203678bf6b63a5f2e7b3e7b77
Dhruv Arora <dhruvarora.code@gmail.com>
2024-01-13 15:52:47 +0530

    implemented static part

--- src/App.js
+++ src/App.js
@@ -15,17 +15,51 @@ export default function App() {
 }
 
 function Logo() {
-  return <div>Logo</div>;
+  return <h1>🌴 Far Away 💼</h1>;
 }
 
 function Form() {
-  return <div>Form</div>;
+  return (
+    <form className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <input type="text" placeholder="Item" />
+      <button>Add</button>
+    </form>
+  );
 }
 
 function PackingList() {
-  return <div>list</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" />
+      <span>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
-  return <div>stats</div>;
+  return <div className="stats">stats</div>;
 }
