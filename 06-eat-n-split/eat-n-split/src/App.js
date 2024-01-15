8ca983e268d030307d7b5b5bd979fbab7b2f54c7
Dhruv Arora <dhruvarora.code@gmail.com>
2024-01-15 20:21:33 +0530

    implemented showaddfriend

--- 06-eat-n-split/eat-n-split/src/App.js
+++ 06-eat-n-split/eat-n-split/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,17 +21,29 @@ const initialFriends = [
   },
 ];
 
-function Button({ children }) {
-  return <button className="button">{children}</button>;
+function Button({ children, onShowAddFriend }) {
+  return (
+    <button className="button" onClick={onShowAddFriend}>
+      {children}
+    </button>
+  );
 }
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onShowAddFriend={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
